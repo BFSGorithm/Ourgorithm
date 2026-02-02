ec5f181133147e752c4c90dd32442e91ce743147
Ronne Mc <dashie@macbookpro.myfiosgateway.com>
2026-02-02 15:51:21 -0500

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // No basePath needed for subdomain
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
